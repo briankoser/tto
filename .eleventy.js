bb1f7396980b453cd7eeb3ef0cc62ad31eb754b5
Brian Koser <brianmkoser@gmail.com>
2019-07-15 22:49:19 -0500

    Add episodes, skeleton files

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
     eleventyConfig.addLayoutAlias('episode', 'layouts/episode.njk');
+    
     eleventyConfig.addFilter("readableDate", dateObj => DateTime.fromJSDate(dateObj).toISODate());
 };
