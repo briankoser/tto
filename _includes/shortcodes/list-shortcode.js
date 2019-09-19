711d8504a48c614dbe846b2e4767c4311df77fcc
Brian Koser <brianmkoser@gmail.com>
2019-09-19 14:26:44 -0500

    Add replaced item to list shortcode

--- _includes/shortcodes/list-shortcode.js
+++ _includes/shortcodes/list-shortcode.js
@@ -2,6 +2,6 @@ module.exports = function listShortCode (host, list) {
     return `
 <h2>${host}</h2>
 <ol reversed>
-${list.map(entry => `<li>${entry}</li>`).join('\n')}
+${list.map(entry => `<li>${entry.replaced == undefined ? '' : '<span style="text-decoration:line-through;">' + entry.replaced + '</span> '}${entry}</li>`).join('\n')}
 </ol>`;
 }
