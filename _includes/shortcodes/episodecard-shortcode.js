6700f6796e3cffe1c2ede3379e19bcd2514afe9e
Brian Koser <brianmkoser@gmail.com>
2019-09-23 16:58:59 -0500

    Add microformats (fixes #6)

--- _includes/shortcodes/episodecard-shortcode.js
+++ _includes/shortcodes/episodecard-shortcode.js
@@ -1,9 +1,9 @@
 module.exports = function episodeCardShortCode (episode, readableDate, cdnUrl) {
     return `
 <article class="episode-card h-entry">
-    <header>
+    <header class="p-name">
         <div class="episode-number">№ ${episode.data.number}</div>
-        <h1 class="p-name">${episode.data.title}</h1>
+        <h1>${episode.data.title}</h1>
         <time class="dt-published" datetime='${episode.date}'>${readableDate}</time>
     </header>
     <div class="e-content">
