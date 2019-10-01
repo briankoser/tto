d9b412668694ebf594a935f1b6b3c897382cd61f
Brian Koser <brianmkoser@gmail.com>
2019-10-01 07:18:36 -0500

    Home page first draft

--- _includes/shortcodes/episodecard-shortcode.js
+++ _includes/shortcodes/episodecard-shortcode.js
@@ -1,14 +1,12 @@
 module.exports = function episodeCardShortCode (episode, readableDate, cdnUrl) {
     return `
-<article class="episode-card h-entry">
-    <header class="p-name">
-        <div class="episode-number">№ ${episode.data.number}</div>
-        <h1>${episode.data.title}</h1>
-        <time class="dt-published" datetime='${episode.date}'>${readableDate}</time>
+<article class="episode episode-card h-entry">
+    <header class="episode">
+        <div class="number">№ ${episode.data.number}</div>
+        <div class="title">${episode.data.title}</div>
     </header>
-    <div class="e-content">
-        <audio src="${cdnUrl}/${episode.fileSlug}.mp3" preload="metadata" controls></audio>
-        <p>${episode.data.description}</p>
+    <div>
+        <p><time class="dt-published" datetime='${episode.date}'>${readableDate}</time> · <span class="e-content">${episode.data.description}</span></p>
     </div>
 </article>`;
 }
