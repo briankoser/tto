7c414411cbe2b3d17aa7c139d9fbbcdc3a8aabd5
Brian Koser <brianmkoser@gmail.com>
2019-10-02 16:18:59 -0500

    Generalize episode block heading

--- _includes/shortcodes/episodeblock-shortcode.js
+++ _includes/shortcodes/episodeblock-shortcode.js
@@ -1,8 +1,8 @@
-module.exports = function episodeBlockShortCode (episode, cdnUrl) {
+module.exports = function episodeBlockShortCode (episode, heading, cdnUrl) {
     return `
 <div class="episode-block js-episode-block">
     <audio src="${cdnUrl}/${episode.fileSlug}.mp3" preload="none"></audio>
-    <div class="latest">Latest</div>
+    <div class="heading">${heading}</div>
     <div class="media-buttons js-media-buttons">
         <div class="play js-media-button">P</div>
         <div class="pause js-media-button hidden">||</div>
