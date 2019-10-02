4859fc69708b398e408b9cf80d950595acc80551
Brian Koser <brianmkoser@gmail.com>
2019-10-02 16:57:08 -0500

    Style home, link hover

--- _includes/shortcodes/episodecard-shortcode.js
+++ _includes/shortcodes/episodecard-shortcode.js
@@ -2,10 +2,12 @@ module.exports = function episodeCardShortCode (episode, readableDate, cdnUrl) {
     return `
 <article class="episode episode-card h-entry">
     <header class="episode">
-        <div class="number">№ ${episode.data.number}</div>
-        <div class="title">${episode.data.title}</div>
+        <a href="${cdnUrl}/${episode.url}" class="title">
+            № ${episode.data.number}<br>
+            ${episode.data.title}
+        </a>
     </header>
-    <div>
+    <div class="summary">
         <p><time class="dt-published" datetime='${episode.date}'>${readableDate}</time> · <span class="e-content">${episode.data.description}</span></p>
     </div>
 </article>`;
