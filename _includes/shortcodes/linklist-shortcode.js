69953b399db170adc4a75129a4af742a7a25bad6
Brian Koser <brianmkoser@gmail.com>
2019-10-09 17:33:35 -0500

    Fix link list shortcode

--- _includes/shortcodes/linklist-shortcode.js
+++ _includes/shortcodes/linklist-shortcode.js
@@ -5,7 +5,7 @@ ${data.host == undefined ? '' : `<h2>${data.host}</h2>`}
 ${data.list.map(entry => 
     `<li>
         ${entry.replaced == undefined ? '' : `<span style="text-decoration:line-through;">${entry.replaced}</span> `}
-        ${entry.url == undefined ? '' : `'<a href="${entry.url}/">${entry.title}</a>`}
+        ${entry.url == undefined ? '' : `<a href="${entry.url}/">${entry.title}</a>`}
     </li>`
 ).join('\n')}
 </ol>`;
