f059c947a534468f2490a2fd78c431b8b4eccef0
Brian Koser <brianmkoser@gmail.com>
2019-10-10 10:49:31 -0500

    Add Youtube notes section, figure to Youtube shortcode

--- _includes/shortcodes/youtube-shortcode.js
+++ _includes/shortcodes/youtube-shortcode.js
@@ -1,8 +1,14 @@
 module.exports = function youtubeShortCode (data) {
-    return `<div class="video-embed-container">
-        ${data.playlistID == undefined ?
-        `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}" frameborder="0" allowfullscreen></iframe>` :
-        `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>}`
+    return `<figure>
+        <div class="video-embed-container">
+            ${data.playlistID == undefined ?
+                `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}" frameborder="0" allowfullscreen></iframe>` :
+                `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>}`
+            }
+        </div>
+        ${data.caption == undefined ?
+            `` :
+            `<figcaption>${data.caption}</figcaption>`
         }
-    </div>`
+    </figure>`
 };
