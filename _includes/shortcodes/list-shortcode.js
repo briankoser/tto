115238874baf1866acda3b269338725402da9184
Brian Koser <brianmkoser@gmail.com>
2019-10-10 13:55:59 -0500

    Remove unused list replaced item

--- _includes/shortcodes/list-shortcode.js
+++ _includes/shortcodes/list-shortcode.js
@@ -3,10 +3,7 @@ module.exports = function listShortCode (data) {
 ${data.host == undefined ? '' : '<h2>' + data.host + '</h2>'}
 <ol reversed>
 ${data.list.map(entry => 
-    `<li>
-        ${entry.replaced == undefined ? '' : `'<span style="text-decoration:line-through;">${entry.replaced}</span> `}
-        ${entry}
-    </li>`
+    `<li>${entry}</li>`
 ).join('\n')}
 </ol>`;
 }
