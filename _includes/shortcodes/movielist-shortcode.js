3683513f6c4906332ab46c982f89f128741d90ab
Brian Koser <brianmkoser@gmail.com>
2019-10-18 17:16:38 -0500

    Add character field to movie list shortcode

--- _includes/shortcodes/movielist-shortcode.js
+++ _includes/shortcodes/movielist-shortcode.js
@@ -5,7 +5,8 @@ ${data.host == undefined ? '' : `<h2>${data.host}</h2>`}
 ${data.list.map(entry => 
     `<li>
     ${entry.replaced == undefined ? '' : `<span style="text-decoration:line-through;">${entry.replaced}</span> `}
-    ${entry.slug == undefined ? `${entry.title}` : `<a href="https://letterboxd.com/film/${entry.slug}/">${entry.title}</a>`}
+    ${entry.character == undefined ? '' : `${entry.character}, `}
+    ${entry.slug == undefined ? `${entry.title}` : `<a href="https://letterboxd.com/film/${entry.slug}/">${entry.character == undefined ? '' : '<i class="movie-title">'}${entry.title}${entry.character == undefined ? '' : '</i>'}</a>`}
     </li>`
 ).join('\n')}
 </ol>`;
