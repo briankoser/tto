c8b9c5094d147d4fcae01751cc13256955bd0d7f
Brian Koser <brianmkoser@gmail.com>
2019-10-23 11:21:04 -0500

    Make playlistId optional for YouTube list

--- _includes/shortcodes/youtubelist-shortcode.js
+++ _includes/shortcodes/youtubelist-shortcode.js
@@ -3,8 +3,7 @@ const youtubeShortcode = require('./youtube-shortcode.js');
 module.exports = function youtubeListShortCode (data) {
     return `
 ${data.host == undefined ? '' : `<h2>${data.host}</h2>`}
-<h3>Playlist</h3>
-${youtubeShortcode(data)}
+${data.playlistID == undefined ? '' : `<h3>Playlist</h3> ${youtubeShortcode(data)}`}
 <ol class="youtubelist" reversed>
 ${data.list.map(entry => 
     `<li>
