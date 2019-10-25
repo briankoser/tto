2fc54e108eda782d112267c4a352bdb98a0a62fb
Brian Koser <brianmkoser@gmail.com>
2019-10-25 11:40:51 -0500

    Add comment to link list shortcode

--- _includes/shortcodes/linklist-shortcode.js
+++ _includes/shortcodes/linklist-shortcode.js
@@ -5,7 +5,8 @@ ${data.host == undefined ? '' : `<h2>${data.host}</h2>`}
 ${data.list.map(entry => 
     `<li>
         ${entry.replaced == undefined ? '' : `<span style="text-decoration:line-through;">${entry.replaced}</span> `}
-        ${entry.url == undefined ? `${entry.title}` : `<a href="${entry.url}/">${entry.title}</a>`}
+        ${entry.url == undefined ? `${entry.title}` : `<a href="${entry.url}/">${entry.title}</a> `}
+        ${entry.comment == undefined ? '' : ` - ${entry.comment}`}
     </li>`
 ).join('\n')}
 </ol>`;
