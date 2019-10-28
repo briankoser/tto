03bc4a9484837add0732b76b2f1e79d9bd16d9a5
Brian Koser <brianmkoser@gmail.com>
2019-10-28 14:56:29 -0500

    Handle list items with no vid in YouTube shortcode

--- _includes/shortcodes/youtube-shortcode.js
+++ _includes/shortcodes/youtube-shortcode.js
@@ -1,15 +1,36 @@
 module.exports = function youtubeShortCode (data) {
+    let videoEmbed = '';
+    if (data.id == undefined && data.playlistID == undefined) {
+        videoEmbed = '<p>No video available</p>';
+    }
+    else if (data.id != undefined) {
+        videoEmbed = `
+<div class="video-embed-container">
+    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}${data.timestamp == undefined ? '' : `?t=${timestampToYouTubeTimestamp(data.timestamp)}`}" frameborder="0" allowfullscreen></iframe>
+</div>`;
+    }
+    else if (data.playlistID != undefined) {
+        videoEmbed = `
+<div class="video-embed-container">
+    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>}
+</div>`;
+    }
+
     return `
 <figure>
-    <div class="video-embed-container">
-        ${data.playlistID == undefined ?
-            `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/${data.id}" frameborder="0" allowfullscreen></iframe>` :
-            `<iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>}`
-        }
-    </div>
-    ${data.caption == undefined ?
-        `` :
-        `<figcaption>${data.caption}</figcaption>`
+    ${videoEmbed}
+    ${data.caption == undefined 
+        ? '' 
+        : `
+<figcaption>
+    ${data.caption} 
+    ${data.timestamp == undefined ? '' : `(${data.timestamp})`}
+</figcaption>`
     }
 </figure>`
+};
+
+let timestampToYouTubeTimestamp = function(timestamp) {
+    let pieces = timestamp.split(':');
+    return `${pieces[0]}m${pieces[1]}s`;
 };
