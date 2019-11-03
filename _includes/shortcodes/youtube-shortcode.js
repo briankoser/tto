020382a6cb4023a7ca33c56f2e3336f1dc5f6a25
Brian Koser <brianmkoser@gmail.com>
2019-11-02 22:33:23 -0500

    Remove unnecessary }

--- _includes/shortcodes/youtube-shortcode.js
+++ _includes/shortcodes/youtube-shortcode.js
@@ -12,7 +12,7 @@ module.exports = function youtubeShortCode (data) {
     else if (data.playlistID != undefined) {
         videoEmbed = `
 <div class="video-embed-container">
-    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>}
+    <iframe class="video-embed" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${data.playlistID}" frameborder="0" allowfullscreen></iframe>
 </div>`;
     }
 
