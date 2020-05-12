087a83d41e6f93e52b85993c42e8ab81b1fd9559
Brian Koser <brianmkoser@gmail.com>
2020-05-12 12:11:30 -0500

    Fix cdnImage shortcode url

--- _includes/shortcodes/cdnimage-shortcode.js
+++ _includes/shortcodes/cdnimage-shortcode.js
@@ -1,7 +1,7 @@
 module.exports = function cdnImageShortCode (image, cdnUrl, alt, caption) {
     return `
 <figure>
-    <img src="${cdnUrl}/img/${image}" alt="${alt}">
+    <img src="${cdnUrl}/${image}" alt="${alt}">
     ${caption == undefined ?
         `` :
         `<figcaption>${caption}</figcaption>`
