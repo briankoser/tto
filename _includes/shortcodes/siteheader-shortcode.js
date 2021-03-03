5bd0fe8c26df2a76987084132bd337ba756fe369
Brian Koser <brianmkoser@gmail.com>
2021-03-03 15:57:24 -0600

    Site header img alt

--- _includes/shortcodes/siteheader-shortcode.js
+++ _includes/shortcodes/siteheader-shortcode.js
@@ -3,7 +3,7 @@ const iconShortcode = require('./icon-shortcode.js');
 module.exports = function siteHeaderShortCode (layout) {
     return `
         <header class="site-header layout-${layout}">
-            <img class="site-logo" src="/img/logo.svg">
+            <img class="site-logo" src="/img/logo.svg" alt="Ten to One logo">
             <h1 class="masthead">Ten to One</h1>
             <a id="site-nav-link" href="#site-nav" class="menu-link js-site-nav">${iconShortcode('menu-hamburger')} Menu</a>
         </header>`;
