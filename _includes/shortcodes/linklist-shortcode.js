ad1fe7f79e881768530b35cb9231dfb392c69118
Brian Koser <brianmkoser@gmail.com>
2021-05-01 22:19:52 -0500

    Ep 143

--- _includes/shortcodes/linklist-shortcode.js
+++ _includes/shortcodes/linklist-shortcode.js
@@ -5,7 +5,7 @@ ${data.host == undefined ? '' : `<h2>${data.host}</h2>`}
 ${data.list.map(entry => 
     `<li>
         ${entry.replaced == undefined ? '' : `<del>${entry.replaced}</del> `}
-        ${entry.url == undefined ? `${entry.title}` : `<a href="${entry.url}/">${entry.title}</a> `}
+        ${entry.url == undefined ? `${entry.title}` : `<a href="${entry.url}">${entry.title}</a> `}
         ${entry.comment == undefined ? '' : ` - ${entry.comment}`}
     </li>`
 ).join('\n')}
